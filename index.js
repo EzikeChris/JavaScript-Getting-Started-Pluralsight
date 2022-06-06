07326707dc7a0cc6bda37fff2cfce3824ca051ff
Chris <christophermbe1009@gmail.com>
2022-06-06 12:07:34 +0100

    Chore:Scope&Hoisting

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function ShowMeter() {
+    let product = 1234;
+
+
+
+    console.log();
+
+    function meterCheck() {
+        let product = 1234;
+        console.log("Available:" , product)
+    }
+
+
+    meterCheck();
+}
+
+showMessage ("Welcome")
